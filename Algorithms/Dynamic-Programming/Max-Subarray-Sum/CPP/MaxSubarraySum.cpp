e85db643de8c53fdc5b704e6851e52d6420eeeb1
tdbrns <tdbarnes00@gmail.com>
2024-11-18 16:49:06 -0600

    Edited comments for Max Subarray Sum

--- Algorithms/Dynamic-Programming/Max-Subarray-Sum/CPP/MaxSubarraySum.cpp
+++ Algorithms/Dynamic-Programming/Max-Subarray-Sum/CPP/MaxSubarraySum.cpp
@@ -22,9 +22,9 @@ using std::max;
 
 int maxSubarraySum (vector<int>& arr)
 {
-    // result and maxValue are intitialized with the element arr[0]
-    int result = arr[0];        // Sum of the maximum subarray in arr[], initialized with arr[0]
+    // result and maxValue are intitialized with element arr[0].
     int maxValue = arr[0];      // Largest value between the sum of the current subarray and element arr[i], initialized with arr[0].
+    int result = arr[0];        // Sum of the maximum subarray in arr[], initialized with arr[0].
 
     for (int i = 1; i < arr.size(); i++) 
     {
@@ -34,17 +34,17 @@ int maxSubarraySum (vector<int>& arr)
 
     return result;
 }
-
-/* maxSubarraySum() when arr = {3, 2, -1, 7, -8, 3, 2}:
-    init.   maxValue = 2; result = 2
-    1.      arr[i] = 3; maxValue = 2+3 = 5; result = 5
-    2.      arr[i] = -8; maxValue = 5+(-8) = -3; result = 5
-    3.      arr[i] = 7; maxValue = (-3)+7 = 4; result = 7  <- (maxValue + arr[i]) < arr[i]; start new subarray with 7 as 1st element
-    4.      arr[i] = -1; maxValue = 7+(-1) = 6; result = 7
-    5.      arr[i] = 2; maxValue = 6+2 = 8; result = 8
-    6.      arr[i] = 3; maxValue = 8+3 = 11; result = 11
+/* maxSubarraySum when arr = {3, 2, -1, 7, -8, 3, 2}
+    init.   maxValue = 3   result = 3
+    1.      arr[i] = 2   maxValue = max(3+2, 3) = 5       result = max(5, 3) = 5
+    2.      arr[i] = -1  maxValue = max(5+(-1), -1) = 4   result = max(5, 4) = 5
+    3.      arr[i] = 7   maxValue = max(4+7, 7) = 11      result = max(11, 5) = 11
+    4.      arr[i] = -8  maxValue = max(11+(-8), -8) = 3  result = max(3, 11) = 11
+    5.      arr[i] = 3   maxValue = max(3+3, 3) = 6       result = max(6, 11) = 11
+    6.      arr[i] = 2   maxValue = max(6+2, 2) = 8       result = max(8, 11) = 11
     return 11
 */
+
 int main()
 {
     vector<int> numArray = {3, 2, -1, 7, -8, 3, 2};
