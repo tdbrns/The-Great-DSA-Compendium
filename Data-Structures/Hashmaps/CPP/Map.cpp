f1d1a18fcbb3d88e7ca1ffaab7178ef22fe36704
tdbrns <tdbarnes00@gmail.com>
2025-01-10 15:09:46 -0600

    Made C++ code for hashmaps more readable

--- Data-Structures/Hashmaps/CPP/Map.cpp
+++ Data-Structures/Hashmaps/CPP/Map.cpp
@@ -11,6 +11,7 @@ Solution:           Use an ordered hash map, which stores key-value pairs in a s
 Resources:          https://cplusplus.com/reference/map/map/
 */
 
+
 #include <iostream>
 #include <map>
 #include <sstream>
@@ -21,160 +22,108 @@ using std::pair;
 using std::string;
 using std::stringstream;
 
-template <class K, class V>
-class OrderedMap
+int main() 
 {
-private:
-    map<K, V> _mp;
+    /***********************************************************************************************************************************/
+    // 1. Create an ordered map (mp) that has char keys and int values.
+    map<char, int> mp;
 
-public:
-    OrderedMap() {
-        initializeMap();
-    }
 
-    void initializeMap(T data)
-    {
-        
-    }
-};
-
-// Insert char-int key-value pairs into an unordered hash map.
-void initializeMap(map<char, int>& mp, string str)
-{
-    // Each key is a character in str; each value is the index of the character in str.
-    for (int i = 0; i < str.size(); i++)
+    /***********************************************************************************************************************************/
+    // 2. Store each character from a string into the map as a key; store each character's index in the string as a value. 
+    string str = "Hello";
+    for (int i = 0; i < str.length(); i++)
         mp[str[i]] = i;
     cout << "Created hash map with " << mp.size() << " elements." << endl;
-}
+    cout << endl;
 
-// Create a temporary key-value pair and copy it into a hash map.
-void insertPair(map<char, int>& mp, char key, int val)
-{
+
+    /***********************************************************************************************************************************/
+    // 3. Create a temporary key-value pair and copy it into mp.
+    char key = '!';
+    int val = 5;
     pair<char, int> newPair (key, val); 
     mp.insert(newPair);
-    
     // Another way...
     //uMp.insert(std::make_pair(key, val));
-}
 
-// Insert a new key-value pair into a hash map without creating a temporary key-value pair.
-void emplacePair(map<char, int>& mp, char key, int val)
-{
+
+    /***********************************************************************************************************************************/
+    // 4. Insert a new key-value pair into mp without creating a temporary key-value pair.
+    key = 'v';
+    val = 6;
     mp.emplace(key, val);
-}
 
-// Check if a specific key in a hash map have has a specific value.
-void checkValue(map<char, int>& mp, char key, int targetVal)
-{
-    // NOTE: if key does not exist in mp, it will automatically be added to mp with value '0'.
-    if (mp[key] == targetVal)
-        cout << "'" << key << "' does have the corresponding value '" << targetVal << "'" << endl;
+
+    /***********************************************************************************************************************************/
+    // 5. Check if a specific key in mp have has a specific value. If key does not exist in mp, it will automatically be added to 
+    //    mp with value 0.
+    char targetKey = 'v';
+    int targetVal = 6;
+    if (mp[targetKey] == targetVal)
+        cout << "'" << targetKey << "' has the corresponding value '" << targetVal << "'" << endl;
     else
-        cout << "'" << key << "' does not have the corresponding value '" << targetVal << "'" << endl;
-}
+        cout << "'" << targetKey << "' does not have the corresponding value '" << targetVal << "'" << endl;
+    cout << endl;
 
-// Print out the contents of a hash map.
-void printMap(map<char, int>& mp)
-{
-    // Print each key-value pair in mp. 
-    // NOTE: itr is an iterator for mp of type map<char, int>::iterator
+
+    /***********************************************************************************************************************************/
+    // 6. Print out each key-value pair in mp. "itr" is and iterator for std::map with the type map<char, int>::iterator.
     cout << "Hash map:" << endl;
     for (auto itr = mp.begin(); itr != mp.end(); itr++)
         cout << "["<< itr->first << ":" << itr->second << "]" << endl;
-}
+    cout << endl;
 
-// Determine if a specific key exists within the hash map and remove it.
-void removePair(map<char, int>& mp, char targetKey)
-{
-    // Search for the target key in the unordered hash map. If found, remove it with erase().
-    // NOTE: find() returns an iterator of type map<char, int>::iterator.
-    if (mp.find(targetKey) != mp.end())
-    {
+
+    /***********************************************************************************************************************************/
+    // 7. Determine if a specific key exists within the mp and remove it. Search for the target key in mp; if found, remove it with 
+    //    erase(). find() returns an iterator of type map<char, int>::iterator.
+    targetKey = 'v';
+    if (mp.find(targetKey) != mp.end()) {
         mp.erase(targetKey);
         cout << "Found and removed element with key '" << targetKey << "'" << endl;
     }   
     else 
         cout << "Could not find element with key '" << targetKey << "'" << endl;
-}
+    cout << endl;
 
-// Determine if a specific key exists within the hash map. If it does extract its node handle from the hash map and then put it 
-// back into the hash map.
-void extractPair(map<char, int>& mp, char targetKey)
-{
-    if (mp.find(targetKey) != mp.end())
-    {
-        auto nodeHandle = mp.extract(targetKey);
-        cout << "Extracted element [" << nodeHandle.key() << ":" << nodeHandle.mapped() <<"]" << endl;
 
-        // Put the node handle back into mp with move().
-        mp.insert(std::move(nodeHandle));
-    }
-    else
-        cout << "Could not find element with key '" << targetKey << "'" << endl;
-}
+    /***********************************************************************************************************************************/
+    // 8. Remove all key-value pairs from mp.
+    mp.clear();
 
-// Determine the frequency of individual characters in a string.
-void printCharFrequency(map<char, int>& mp, string str)
-{
+
+    /***********************************************************************************************************************************/
+    // 9. Determine the frequency of characters in a string (i.e., count the number of times a each character appears in a string).
+    str = "abcba";
+    map<char, int> charHz;            // Stores each character in str as a key and the frequency of each character as a value.
+
     // Iterate through each character in str; if a character appears more than once, add 1 to its frequency value.
     for (int i = 0; i < str.length(); i++)
-        mp[str[i]]++;
+        charHz[str[i]]++;
     
     // Print each character that is in str along with its frequency value.
     cout << "Character frequency:" << endl;
-    for (auto itr = mp.begin(); itr != mp.end(); itr++)
+    for (auto itr = charHz.begin(); itr != charHz.end(); itr++)
         cout << "[" << itr->first << ":" << itr->second << "]" << endl;
-}
+    cout << endl;
 
-// Determine the frequency of individual words in a string.
-void printWordFrequency(map<string, int>& mp, string str)
-{
-    stringstream ss(str);       // Used to break down the string into words (substrings separated by spaces).
-    string word;                // An individual word from str2.
+
+    /***********************************************************************************************************************************/
+    // 10. Determine the frequency of individual words in a string.
+    str = "die die die death destruction turmoil turmoil death die";
+    map<string, int> wordHz;      // Stores each word in str as a key and the frequency of each word as a value.
+    stringstream ss(str);           // Used to break down the string into words (substrings separated by spaces).
+    string word;                    // An individual word from str2.
 
     // Iterate through each word in str; if a word appears more than once, add 1 to its frequency value.
     while (ss >> word)
-        mp[word]++;
+        wordHz[word]++;
     
     // Print each word that is in str along with its frequency value.
     cout << "Word frequency:" << endl;
-    for (auto itr = mp.begin(); itr != mp.end(); itr++)
+    for (auto itr = wordHz.begin(); itr != wordHz.end(); itr++)
         cout << "[" << itr->first << ":" << itr->second << "]" << endl;
-}
-
-int main()
-{
-    map<char, int> mp;                  // Stores characters as keys and their corresponding values are integers.
-    initializeMap(mp, "Hello");         // Fill mp with each character in "Hello".
-    cout << endl;
-    
-    insertPair(mp, '!', 5);
-
-    emplacePair(mp, '?', 6);
-
-    checkValue(mp, 'v', 7);
-    cout << endl;
-
-    extractPair(mp, '!');
-    cout << endl;
-
-    printMap(mp);
-    cout << endl;
-
-    removePair(mp, 'v');                // Remove element with key 'v' from mp.
-    removePair(mp, 'v');                // Remove element with key 'v' from mp again.
-    cout << endl;
-
-    mp.clear();                         // Remove all key-value pairs from mp.
-
-    string str = "abcba";
-    map<char, int> charFreq;            // Stores each character in str as a key and the frequency of each word as a value.
-    printCharFrequency(charFreq, str);
-    cout << endl;
-
-    str = "die die die death destruction turmoil turmoil death die";
-    map<string, int> wordFreq;    // Stores each word in str as a key and the frequency of each word as a value.
-    printWordFrequency(wordFreq, str);
     cout << endl;
 
     return 0;
--- Data-Structures/Hashmaps/CPP/UnorderedMap.cpp
+++ Data-Structures/Hashmaps/CPP/UnorderedMap.cpp
@@ -11,6 +11,7 @@ Resources:          https://en.cppreference.com/w/cpp/container/unordered_map
                     https://www.geeksforgeeks.org/map-vs-unordered_map-c/
 */
 
+
 #include <iostream>
 #include <unordered_map>
 #include <sstream>
@@ -21,158 +22,113 @@ using std::pair;
 using std::string;
 using std::stringstream;
 
-// Insert char-int key-value pairs into a hash map.
-void initializeMap(unordered_map<char, int>& uMp, string str)
+int main()
 {
-    // Store the characters of str as key-value pairs in uMp.
-    // Each key is a character in str; each value is the index of the character in str.
+    /***********************************************************************************************************************************/
+    // 1. Create an unordered map (uMp) that has char keys and int values.
+    unordered_map<char, int> uMp;
+
+
+    /***********************************************************************************************************************************/
+    // 2. Store each character from a string into uMp as a key; store each character's index in the string as a value. 
+    string str = "Hello";
     for (int i = 0; i < str.length(); i++)
         uMp[str[i]] = i;
     cout << "Created hash map with " << uMp.size() << " elements." << endl;
-}
+    cout << endl;
 
-// Print the number of buckets in a hash map and bucket in which each element of the hash map has been placed.
-// NOTE: a hash function is performed on each element to determine the bucket it will be placed into.
-// NOTE: The bucket count of a hash map will always increase to accommodate the number of elements being inserted.
-void printBucketInfo(unordered_map<char, int>& uMp)
-{
+
+    /***********************************************************************************************************************************/
+    // 3. Print the number of buckets in uMp and the bucket in which each element of uMp has been placed. A hash function is performed 
+    //    on each element to determine the bucket it will be placed into. The bucket count of a hash map will always increase to 
+    //    accommodate the number of elements being inserted.
     cout << "Bucket count: " << uMp.bucket_count() << endl;
-    
     for (auto itr = uMp.begin(); itr != uMp.end(); itr++)
         cout << "Element [" << itr->first << ":" << itr->second << "] is in bucket #" << uMp.bucket(itr->first) << endl;
-}
+    cout << endl;
 
-// Create a temporary key-value pair and copy it into a hash map.
-void insertPair(unordered_map<char, int>& uMp, char key, int val)
-{
+
+    /***********************************************************************************************************************************/
+    // 4. Create a temporary key-value pair and copy it into uMp.
+    char key = '!';
+    int val = 5;
     pair<char, int> newPair (key, val); 
     uMp.insert(newPair);
-
     // Another way...
     //uMp.insert(std::make_pair(key, val));
-}
 
-// Insert a new key-value pair into a hash map without creating a temporary key-value pair.
-void emplacePair(unordered_map<char, int>& uMp, char key, int val)
-{
+
+    /***********************************************************************************************************************************/
+    // 5. Insert a new key-value pair into uMp without creating a temporary key-value pair.
+    key = '?';
+    val = 6;
     uMp.emplace(key, val);
-}
 
-// Check if a specific key in a hash map have has a specific value.
-void checkValue(unordered_map<char, int>& uMp, char key, int targetVal)
-{
-    // NOTE: if key does not exist in uMp, it will automatically be added to uMp with value '0'.
-    if (uMp[key] == targetVal)
-        cout << "'" << key << "' does have the corresponding value '" << targetVal << "'" << endl;
+    /***********************************************************************************************************************************/
+    // 6. Check if a specific key in uMp have has a specific value. If key does not exist in mp, it will automatically be added to 
+    //    uMp with value 0.
+    char targetKey = '?';
+    int targetVal = 6;
+    if (uMp[targetKey] == targetVal)
+        cout << "'" << targetKey << "' has the corresponding value '" << targetVal << "'" << endl;
     else
-        cout << "'" << key << "' does not have the corresponding value '" << targetVal << "'" << endl;
-}
+        cout << "'" << targetKey << "' does not have the corresponding value '" << targetVal << "'" << endl;
+    cout << endl;
 
-// Print out the contents of a hash map.
-void printMap(unordered_map<char, int>& uMp)
-{
-    // Print each key-value pair in uMp. 
-    // NOTE: itr is an iterator for uMp of type unordered_map<char, int>::iterator
+
+    /***********************************************************************************************************************************/
+    // 7. Print out each key-value pair in mp. "itr" is and iterator for std::unordered_map with the type 
+    //    unordered_map<char, int>::iterator.
     cout << "Hash map:" << endl;
     for (auto itr = uMp.begin(); itr != uMp.end(); itr++)
         cout << "["<< itr->first << ":" << itr->second << "]" << endl;
-}
+    cout << endl;
 
-// Determine if a specific key exists within the hash map and remove it.
-void removePair(unordered_map<char, int>& uMp, char targetKey)
-{
-    // Search for the target key in the unordered hash map. If found, remove it with erase().
-    // NOTE: find() returns an iterator of type unordered_map<char, int>::iterator.
-    if (uMp.find(targetKey) != uMp.end())
-    {
+
+    /***********************************************************************************************************************************/
+    // 8. Determine if a specific key exists within the uMp and remove it. Search for the target key in uMp; if found, remove it with 
+    //    erase(). find() returns an iterator of type unordered_map<char, int>::iterator.
+    targetKey = '?';
+    if (uMp.find(targetKey) != uMp.end()) {
         uMp.erase(targetKey);
         cout << "Found and removed element with key '" << targetKey << "'" << endl;
     }   
     else 
         cout << "Could not find element with key '" << targetKey << "'" << endl;
-}
+    cout << endl;
 
-// Determine if a specific key exists within the hash map. If it does extract its node handle from the hash map and then put it 
-// back into the hash map.
-void extractPair(unordered_map<char, int>& uMp, char targetKey)
-{
-    if (uMp.find(targetKey) != uMp.end())
-    {
-        auto nodeHandle = uMp.extract(targetKey);
-        cout << "Extracted element [" << nodeHandle.key() << ":" << nodeHandle.mapped() <<"]" << endl;
 
-        // Put the node handle back into uMp with move().
-        uMp.insert(std::move(nodeHandle));
-    }
-    else
-        cout << "Could not find element with key '" << targetKey << "'" << endl;
-}
+    /***********************************************************************************************************************************/
+    // 9. Determine the frequency of characters in a string (i.e., count the number of times a each character appears in a string).
+    str = "abcba";
+    unordered_map<char, int> charHz;            // Stores each character in str as a key and the frequency of each character as a value.
 
-// Determine the frequency of individual characters in a string.
-void printCharFrequency(unordered_map<char, int>& uMp, string str)
-{
     // Iterate through each character in str; if a character appears more than once, add 1 to its frequency value.
     for (int i = 0; i < str.length(); i++)
-        uMp[str[i]]++;
+        charHz[str[i]]++;
     
     // Print each character that is in str along with its frequency value.
     cout << "Character frequency:" << endl;
-    for (auto itr = uMp.begin(); itr != uMp.end(); itr++)
+    for (auto itr = charHz.begin(); itr != charHz.end(); itr++)
         cout << "[" << itr->first << ":" << itr->second << "]" << endl;
-}
+    cout << endl;
 
-// Determine the frequency of individual words in a string.
-void printWordFrequency(unordered_map<string, int>& uMp, string str)
-{
-    stringstream ss(str);       // Used to break down the string into words (substrings separated by spaces).
-    string word;                // An individual word from str2.
+
+    /***********************************************************************************************************************************/
+    // 10. Determine the frequency of individual words in a string.
+    str = "die die die death destruction turmoil turmoil death die";
+    unordered_map<string, int> wordHz;      // Stores each word in str as a key and the frequency of each word as a value.
+    stringstream ss(str);                   // Used to break down the string into words (substrings separated by spaces).
+    string word;                            // An individual word from str2.
 
     // Iterate through each word in str; if a word appears more than once, add 1 to its frequency value.
     while (ss >> word)
-        uMp[word]++;
+        wordHz[word]++;
     
     // Print each word that is in str along with its frequency value.
     cout << "Word frequency:" << endl;
-    for (auto itr = uMp.begin(); itr != uMp.end(); itr++)
+    for (auto itr = wordHz.begin(); itr != wordHz.end(); itr++)
         cout << "[" << itr->first << ":" << itr->second << "]" << endl;
-}
-
-int main()
-{
-    unordered_map<char, int> uMp;           // Stores each character of str as a key and each character's index in str as a value.
-    initializeMap(uMp, "Hello");            // Fill uMp with each character in "Hello".
-    cout << endl;
-
-    printBucketInfo(uMp);
-    cout << endl;
-
-    insertPair(uMp, '!', 5);
-
-    emplacePair(uMp, '?', 6);
-
-    checkValue(uMp, 'v', 7);
-    cout << endl;
-
-    extractPair(uMp, '!');
-    cout << endl;
-
-    printMap(uMp);
-    cout << endl;
-
-    removePair(uMp, 'v');                   // Remove element with key 'v' from uMp.
-    removePair(uMp, 'v');                   // Remove element with key 'v' from uMp again.
-    cout << endl;
-
-    uMp.clear();                            //  Remove all key-value pairs from uMp.
-
-    string str = "abcba";
-    unordered_map<char, int> charFreq;      // Stores each character in str as a key and the frequency of each word as a value.
-    printCharFrequency(charFreq, str);
-    cout << endl;
-
-    str = "die die die death destruction turmoil turmoil death die";
-    unordered_map<string, int> wordFreq;    // Stores each word in str as a key and the frequency of each word as a value.
-    printWordFrequency(wordFreq, str);
     cout << endl;
 
     return 0;
